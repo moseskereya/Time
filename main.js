3f3f98835e92ed907eb613ac0b0e567116915b05
Moses <lazaromoses77@gmail.com>
2020-02-21 15:38:13 +0300

    adding_more_functions

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
 var date = new Date();
 const hide = document.querySelector('.time')
 const btn = document.querySelector('.show');
+var change = document.querySelector('.span')
 let timeStatus = 1;
 btn.onclick = () =>{
     if(timeStatus === 1){
         var mytime = document.querySelector('.time').innerHTML = date;
         hide.style.display = 'block'
+        change.innerHTML = 'Hide'
         timeStatus = 0;
     }else{
         hide.style.display = 'none'
+        change.innerHTML = 'Show'
          timeStatus = 1;
+
     }
   
 }
